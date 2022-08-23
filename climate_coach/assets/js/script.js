145971e3d276fb55980a62211806b224855645c9
Huilian Sophie Qiu <hsqq@cmu.edu>
2022-08-23 17:46:48 +0800

    revert date range, move to plot_default

--- climate_coach/assets/js/script.js
+++ climate_coach/assets/js/script.js
@@ -415,21 +415,10 @@ var button_i_5 = document.getElementById("5");
 var button_i_4 = document.getElementById("4");
 var button_i_3 = document.getElementById("3");
 
-num_months = months.length;
-button_i_6.innerHTML = months[num_months-1];
-button_i_5.innerHTML = months[num_months-2];
-button_i_4.innerHTML = months[num_months-3];
-button_i_3.innerHTML = months[num_months-4];
-
 var button_t_6 = document.getElementById("pr_6");
 var button_t_5 = document.getElementById("pr_5");
 var button_t_4 = document.getElementById("pr_4");
 var button_t_3 = document.getElementById("pr_3");
-
-button_t_6.innerHTML = months[num_months-1];
-button_t_5.innerHTML = months[num_months-2];
-button_t_4.innerHTML = months[num_months-3];
-button_t_3.innerHTML = months[num_months-4];
 
 function displayToxic(data, type){
     const toxicMonthSelect = document.querySelectorAll(".issue_toxic_month_selector");
